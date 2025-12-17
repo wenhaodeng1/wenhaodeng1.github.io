beca23176d468f8d2fc4ca4d51607677dd53be8f
wenhaodeng1 <2794698154@qq.com>
2025-12-18 01:59:06 +0800

    Update Web1.js

---
+++ pages/Web1/Web1.js
@@ -0,0 +1,14 @@
+// 点击 projects 跳转到 pages/Web2/Web2.html
+document.getElementById('navProjects').addEventListener('click', function() {
+    window.location.href = 'pages/Web2/Web2.html';
+});
+
+// 点击 blog 跳转到 pages/Web3/Web3.html
+document.getElementById('navBlog').addEventListener('click', function() {
+    window.location.href = 'pages/Web3/Web3.html';
+});
+
+// 点击 CV 跳转到 pages/Web4/Web4.html
+document.getElementById('navCV').addEventListener('click', function() {
+    window.location.href = 'pages/Web4/Web4.html';
+});
